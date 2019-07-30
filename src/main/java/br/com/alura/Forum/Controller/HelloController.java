a3b4870c1234342d90163c1201ee533aed7ca8fd
raulzitoo <raulzito.j@gmail.com>
2019-07-30 16:58:19 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/alura/Forum/Controller/HelloController.java
@@ -0,0 +1,18 @@
+package br.com.alura.Forum.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String hello() {	
+		
+		
+		return "Hello Wordd"; 
+	}
+
+}
